e31ac75facc6e55d3d837182ea1435a9994f7285
Matthew Du <du.matthew@gmail.com>
2015-10-10 14:22:55 -0400

    Working postmates

--- postmates/postmates.go
+++ postmates/postmates.go
@@ -3,9 +3,9 @@ package postmates
 import (
 	"appengine"
 	"appengine/urlfetch"
-	"bytes"
-	"encoding/json"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 type CreateDeliveryRequest struct {
@@ -28,23 +28,27 @@ const API_KEY string = "5d8dbe7d-1897-4239-b5b3-780c2a3965d5"
 
 func CreateDelivery(c appengine.Context, manifest string, pickup_name string, pickup_address string, pickup_phone_number string, pickup_business_name string, pickup_notes string, dropoff_name string, dropoff_address string, dropoff_phone_number string, dropoff_business_name string, dropoff_notes string) (*http.Response, error) {
 	client := urlfetch.Client(c)
-	url := BASE_URL + "/v1/customers/" + CUSTOMER_ID + "/deliveries"
-	req := CreateDeliveryRequest{
-		Manifest:              manifest,
-		Pickup_name:           pickup_name,
-		Pickup_address:        pickup_address,
-		Pickup_phone_number:   pickup_phone_number,
-		Pickup_business_name:  pickup_business_name,
-		Pickup_notes:          pickup_notes,
-		Dropoff_name:          dropoff_name,
-		Dropoff_address:       dropoff_address,
-		Dropoff_phone_number:  dropoff_phone_number,
-		Dropoff_business_name: dropoff_business_name,
-		Dropoff_notes:         dropoff_notes,
-	}
-	encoded, err := json.Marshal(req)
+	postUrl := BASE_URL + "/v1/customers/" + CUSTOMER_ID + "/deliveries"
+
+	form := url.Values{}
+	form.Add("manifest", manifest)
+	form.Add("pickup_name", pickup_name)
+	form.Add("pickup_address", pickup_address)
+	form.Add("pickup_phone_number", pickup_phone_number)
+	form.Add("pickup_business_name", pickup_business_name)
+	form.Add("pickup_notes", pickup_notes)
+	form.Add("dropoff_name", dropoff_name)
+	form.Add("dropoff_address", dropoff_address)
+	form.Add("dropoff_phone_number", dropoff_phone_number)
+	form.Add("dropoff_business_name", dropoff_business_name)
+	form.Add("dropoff_notes", dropoff_notes)
+
+	req, err := http.NewRequest("POST", postUrl, strings.NewReader(form.Encode()))
 	if err != nil {
-		return nil, err
+		c.Errorf("%s", err)
 	}
-	return client.Post(url, "application/json", bytes.NewBuffer(encoded))
+	req.PostForm = form
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(API_KEY, "")
+	return client.Do(req)
 }
